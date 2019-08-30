67dcccc0e3da722cfc40b3957bb1f39a35abc261
whatever <helloworldagain2@gmail.com>
2019-08-30 13:44:11 +0300

    Initial script

--- iso_language_codes/iso_language_codes.py
+++ iso_language_codes/iso_language_codes.py
@@ -1,3 +1,28 @@
 # -*- coding: utf-8 -*-
+import shelve
 
-"""Main module."""
+
+def language(code):
+    assert (len(code) == 2)  # Please provide a two-letter code
+    code.lower()
+    with shelve.open('language_codes.db', flag='r') as s:
+        return s[f'{code}']
+
+
+def language_name(code):
+    assert (len(code) == 2)  # Please provide a two-letter code
+    code.lower()
+    with shelve.open('language_codes.db', flag='r') as s:
+        return s[f'{code}']['Name']
+
+
+def language_autonym(code):
+    assert (len(code) == 2)  # Please provide a two-letter code
+    code.lower()
+    with shelve.open('language_codes.db', flag='r') as s:
+        return s[f'{code}']['Autonym']
+
+
+def language_dictionary():
+    with shelve.open('language_codes.db', flag='r') as s:
+        return dict(s)
