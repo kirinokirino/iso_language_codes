dafdd356ba782f66b23e9ae28ab6cb2407c25775
whatever <helloworldagain2@gmail.com>
2019-09-02 17:08:23 +0300

    Bump version: 1.0.5 → 1.0.6

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kirinokirino/iso_language_codes',
-    version='1.0.5',
+    version='1.0.6',
     zip_safe=False,
 )
